90ddd347ed1a75442337d2f36b2c174f218c3607
james-meyer-784110 <james.meyer@cognizant.com>
2020-05-20 16:07:36 -0500

    initial react app

---
+++ product_ui/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>Product Availability</h1>
+          <span>v0.0.1</span>
+          <div>srouce</div>
+          <div>By James Meyer</div>
+        </p>
+        <div>
+          <a href="site">site</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
